a329070145a3ed64256f8d8781c387b4f48548f2
camark <camark@live.cn>
2016-09-21 13:56:43 +0800

    add route

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,23 +1,35 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { routing } from './app.routing';
 
 import { AppComponent }   from './app.component';
 import { StudentComponent }   from './app.student';
-import {HeroDetailComponent} from './hero-detail.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    FormsModule 
+    FormsModule,
+    routing
   ],
   declarations: [ 
     AppComponent,
+    DashboardComponent,
     StudentComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap:    [ 
+    AppComponent
+  ]
 })
 
 export class AppModule { }
